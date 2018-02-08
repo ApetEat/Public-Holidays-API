0a90448d750d7846fd84dd4805bd753ac194dab0
Sergio de Diego <sergio.dediego@apeteat.es>
2018-02-08 19:01:24 +0100

    Change url regex pattern

--- PublicHolidaysAPI/publicholidays/urls.py
+++ PublicHolidaysAPI/publicholidays/urls.py
@@ -1,13 +1,11 @@
-from django.urls import path, re_path, register_converter
+from django.urls import re_path
 
-from . import converters, views
+from . import views
 
-
-register_converter(converters.DateConverter, 'yyyymmdd')
 
 urlpatterns = [
     re_path(
-        r'^(?P<date>\d{4}[.-/]\d{1,2}[.-/]\d{1,2})/(?P<locality>\d+)/(?:(?P<province>\d+)/)?(?:(?P<community>\d+)/)?(?:(?P<country>\d+)/)?$',
+        r'^(?P<date>\d{4}-\d{1,2}-\d{1,2})/(?P<locality_id>\d+)/(?:(?P<province_id>\d+)/)?(?:(?P<community_id>\d+)/)?(?:(?P<country_id>\d+)/)?$',
         views.check_public_holiday,
         name='public-holiday',
     ),
