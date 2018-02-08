3aa9fef612655c64621f8a0a558419cdf01a1782
Sergio de Diego <sergio.dediego@apeteat.es>
2018-02-08 19:37:54 +0100

    Format too long url regex

--- PublicHolidaysAPI/publicholidays/urls.py
+++ PublicHolidaysAPI/publicholidays/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 urlpatterns = [
     re_path(
-        r'^(?P<date>\d{4}-\d{1,2}-\d{1,2})/(?P<locality_id>\d+)/(?:(?P<province_id>\d+)/)?(?:(?P<community_id>\d+)/)?(?:(?P<country_id>\d+)/)?$',
+        r'^(?P<date>\d{4}-\d{1,2}-\d{1,2})/'
+        r'(?P<locality_id>\d+)/'
+        r'(?:(?P<province_id>\d+)/)?'
+        r'(?:(?P<community_id>\d+)/)?'
+        r'(?:(?P<country_id>\d+)/)?$',
         views.check_public_holiday,
         name='public-holiday',
     ),
