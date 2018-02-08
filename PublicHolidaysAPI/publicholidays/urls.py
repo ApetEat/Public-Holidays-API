85f0893f013572c59ef919a6e8550d3d4c463e31
Sergio de Diego <sergio.dediego@apeteat.es>
2018-02-08 16:19:11 +0100

    Fix url regex for several optional parameters

--- PublicHolidaysAPI/publicholidays/urls.py
+++ PublicHolidaysAPI/publicholidays/urls.py
@@ -2,20 +2,13 @@ from django.urls import path, re_path, register_converter
 
 from . import converters, views
 
-# Create your urls here.
-
 
 register_converter(converters.DateConverter, 'yyyymmdd')
 
 urlpatterns = [
-    path(
-        '<yyyymmdd:date>/<int:locality>/<int:province>/<int:community>/<int:country>',
+    re_path(
+        r'^(?P<date>\d{4}[.-/]\d{1,2}[.-/]\d{1,2})/(?P<locality>\d+)/(?:(?P<province>\d+)/)?(?:(?P<community>\d+)/)?(?:(?P<country>\d+)/)?$',
         views.check_public_holiday,
         name='public-holiday',
-    ),
-    re_path(
-        r'(?P<date>\d{4}/\d{1,2}/\d{1,2})/(?P<locality>\d+)/(?:/(?P<province>\d+))?/(?:/(?P<community>\d+))?/(?:/(?P<country>\d+))?/',
-        views.check_public_holiday,
-        name='re-public-holiday',
     ),
 ]
